6f25e6a8f8ad5920bd01b816a763d59eb5ac57b5
Taron Gukasyan <funkadelica@mail.ru>
2023-06-30 21:07:49 +0400

    added after*(AMENDED)

--- Runner.java
+++ Runner.java
@@ -1,6 +1,7 @@
 public class Runner {
     public static void main(String[] args) {
         int master;
+        int afterREBASED;
     }
    private static void ter(){
         System.out.println("ter methods");
