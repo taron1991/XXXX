38c8899642ff4cfe4a815383909f8a3bc5b0d258
Taron Gukasyan <funkadelica@mail.ru>
2023-06-30 21:26:41 +0400

    added int in master

--- Runner.java
+++ Runner.java
@@ -2,6 +2,7 @@ public class Runner {
     public static void main(String[] args) {
         int master;
         int afterREBASED;
+        int masterBarnch;
     }
    private static void ter(){
         System.out.println("ter methods");
